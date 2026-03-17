679b1cc70e1ca6511442fc44b04cd1f65f093c77
Eric Lourenço <ericloumendes@gmail.com>
2026-03-17 08:04:25 -0300

    feat: Adicionando pipeline de CI feat: Adicionando template padrão do frontend
    No ramo develop
    Mudanças a serem submetidas:
    - new file:   .github/pull_request_template.md
    - new file:   .github/workflows/flutter_ci.yml

---
+++ test/smoke_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('basic widget test', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Text('Hello CI'),
+      ),
+    );
+
+    expect(find.text('Hello CI'), findsOneWidget);
+  });
+}
