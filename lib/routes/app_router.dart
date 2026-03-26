e8d439b4d7bcefbfd7c1a6a6790a89cdc8c658c1
Brenno <brennolyrio@gmail.com>
2026-03-25 22:48:01 -0300

    VER-15 feat: precedentSuggestedCard mais Precedents

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../features/account/presentation/login/screens/login_screen.dart';
 import '../features/account/presentation/profile/screens/profile_screen.dart';
+import '../features/precedent/presentation/PrecedentSuggested/screens/teste.dart'; // arrancar depois 
 
 import '../features/petition/presentation/petition_history/screens/petition_history_screen.dart';
 import '../features/petition/presentation/petition_upload/screens/petition_upload_screen.dart';
@@ -13,12 +14,14 @@ class AppRouter {
   static const petitionUpload = '/petition_upload';
   static const profile = '/profile';
   static const petitionHistory = '/petition_history';
+  static const precedentSuggestedTest = '/precedent_suggested_test'; // arrancar depois 
 
   static final Set<String> publicRoutes = {
     login,
     petitionUpload,
     profile,
     petitionHistory,
+    precedentSuggestedTest, // arrancar depois 
   };
 
   static const List<AppBottomNavItem> homeBottomItems = [
@@ -37,6 +40,11 @@ class AppRouter {
       icon: Icons.person_rounded,
       route: profile,
     ),
+    AppBottomNavItem( // arrancar depois
+    label: 'Teste',
+    icon: Icons.science_rounded,
+    route: precedentSuggestedTest,
+  ),
   ];
 
   static int _indexForRoute(String route) {
@@ -61,6 +69,8 @@ class AppRouter {
     switch (routeName) {
       case login:
         return _buildSimpleRoute(child: const LoginScreen());
+      case precedentSuggestedTest: // arrancar depois 
+        return _buildSimpleRoute(child: const PrecedentSuggestedTestScreen()); // arrancar depois 
       case petitionUpload:
       case profile:
       case petitionHistory:
@@ -89,6 +99,7 @@ class _HomeTabsShellState extends State<_HomeTabsShell> {
     PetitionUploadScreen(),
     PetitionHistoryScreen(),
     ProfileScreen(),
+    PrecedentSuggestedTestScreen(), // arrancar depois
   ];
 
   @override
