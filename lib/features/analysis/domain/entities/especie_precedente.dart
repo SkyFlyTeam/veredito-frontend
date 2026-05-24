47b438a37707490f27cc1980250fe98b57e7ef83
karengoncalves8 <karengoncalves128@gmail.com>
2026-05-24 00:25:23 -0300

    VER-112 feat: adicionando sheet de tribunais e especies

---
+++ lib/features/analysis/domain/entities/especie_precedente.dart
@@ -0,0 +1,11 @@
+class EspeciePrecedente {
+  final int id;
+  final String nome;
+  final String sigla;
+
+  EspeciePrecedente({
+    required this.id,
+    required this.nome,
+    required this.sigla,
+  });
+}
